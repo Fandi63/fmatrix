bfa7c5407d50fcd8a1e2292e694ca5f0c9461aed
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-14 19:55:20 +0200

    Create fmatrix.py

---
+++ fmatrix.py
@@ -0,0 +1,38 @@
+import os
+import random
+os.system("apt install figlet")
+os.system("figlet FmatriX")
+input("Start Matrix?")
+os.system("clear")
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+print(random.randint(0,10000000000000))
+
+
+
+
+
+
+
+
