c93425b06b87cd3b2e6705121bdb2192393d3cf9
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-15 07:10:13 +0200

    Update fmatrix.py

--- fmatrix.py
+++ fmatrix.py
@@ -1,6 +1,7 @@
 import os
 import random
 os.system("apt install figlet")
+os.system("pkg install cmatrix")
 os.system("clear")
 os.system("figlet FmatriX")
 input("Start Matrix?")
@@ -29,7 +30,8 @@ print(random.randint(0,10000000000000))
 print(random.randint(0,10000000000000))
 print(random.randint(0,10000000000000))
 print(random.randint(0,10000000000000))
-
+os.system("clear")
+os.system("cmatrix")
 
 
 
